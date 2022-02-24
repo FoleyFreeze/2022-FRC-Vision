added debug mode; added network table; fixed hub x,y calculation

vision_hub.py
@@ -11,6 +11,7 @@ import numpy as np
 import PySimpleGUI as sg
 import configparser
 from networktables import NetworkTables
+import sys
 
 DEFAULT_PARAMETERS_FILENAME = "default-params.ini"
 PARAMETERS_FILENAME = "kind of working params(cloudy)"
@@ -30,19 +31,16 @@ VERTICAL_DEGREES_PER_PIXEL = VERTICAL_FOV / PIXEL_HEIGHT
 RIO_IP = "10.9.10.2"
 
 def look_up_distance_y(y_pixel):
-    pass #to do
+    return(0)
 
 def look_up_distance_x(x_width):
-    pass #to do
-
+    return(0)
+    
 def calc_horizontal_angle_of(x_pixel):
     return (x_pixel - HORIZONTAL_PIXEL_CENTER) * HORIZONTAL_DEGREES_PER_PIXEL
 
 def calc_vertical_angle_of(y_pixel):
     return (y_pixel - VERTICAL_PIXEL_CENTER) * VERTICAL_DEGREES_PER_PIXEL
-
-def output_data(d,a_of):
-    pass #to do
 
 def find_min_x(contours):
     min_x = 999
@@ -159,6 +157,21 @@ def read_params_file(file):
     area = config['params_section']['min cargo area']
     cv2.setTrackbarPos("min cargo area","app config",int(area))
 
+def output_data(loops,current_time,calc_time,cam_distance,cam_angle_of_horizontal,color):
+    # data format for network table key "Target" : "id,current time,calcuated time,distance,angle of,color"
+    hub_data = "%d,%8.3f,%8.3f,%8.3f,%8.3f,%d" % (loops,current_time,calc_time,cam_distance,cam_angle_of_horizontal,color)
+    nt.putString("Target",hub_data)
+
+    if debug_mode == True:
+        print( hub_data)
+
+def draw_target(image,pts):
+
+    if debug_mode == True:
+        image = cv2.polylines(image, [pts], True, (0,0,255), 3)
+
+    return(image)
+
 class PiVideoStream: # from pyimagesearch
     def __init__(self, resolution=(PIXEL_WIDTH, PIXEL_HEIGHT), framerate=40, brightness=54, \
         contrast=100, sharpness=100, exposure_compensation=-12, saturation=100):
@@ -225,6 +238,13 @@ def show_x_and_y(event, x, y, flags, userdata):
 
 # START
 
+debug_mode = False
+# determine debug mode depending on how the program was run
+if len(sys.argv) == 2 and sys.argv[1] == "debug":
+    debug_mode = True
+else:
+    debug_mode = False
+
 # create window with trackbars to control the color range
 cv2.namedWindow("app config")
 cv2.resizeWindow("app config",600,800)
@@ -282,7 +302,6 @@ while True:
 
     l = len(contours)
     if (l == 5):
-        print (l)
         # because they are often not fully visible at the far left and far right, drop far left and far right
         c_min_x, p_min_x = find_min_x(contours)
         c_max_x, p_max_x = find_max_x(contours)
@@ -301,15 +320,19 @@ while True:
 
         #Hub center = center of bounding rectangle of min y contours
         x,y,w,h = cv2.boundingRect(contours[c_min_y][p_min_y])
-        hub_x = x + (w/2)
-        hub_y = y + (h/2)
+        hub_x = round(x + (w/2))
+        hub_y = round(y + (h/2))
 
         cam_distance = look_up_distance_y(hub_y) # (if camera is in fixed position)
         #cam_distance = look_up_distance_x(max_x_x - min_x_x) # x-pixel width (if camera is on shooter)
         cam_angle_of_horizontal = calc_horizontal_angle_of(hub_x) # x coordinate of min y-pixel value
         cam_angle_of_vertical = calc_vertical_angle_of(hub_y)
 
-        output_data(cam_distance,cam_angle_of_horizontal)
+        # loop time
+        current_time = time.process_time()
+        calc_time = current_time - start_time
+        loops = loops + 1
+        output_data(loops,current_time,calc_time,cam_distance,cam_angle_of_horizontal,0)
 
         # draw hub target
         x_min_x = contours[c_min_x][p_min_x][0][0]
@@ -319,12 +342,10 @@ while True:
         y_max_y = contours[c_max_x][p_max_x][0][1]
 
         pts = np.array([[x_min_x,y_min_x],[hub_x,hub_y],[x_max_x,y_max_y]], np.int32)
-        image = cv2.polylines(image, [pts], True, (0,0,255), 3)
-        #cv2.circle(image, (x_min_y,y_min_y), 20, (0,0,255), 3)
+        image = draw_target(image,pts)
 
     elif (l == 4):
 
-        print (l)
         # start with the far left and far right
         c_min_x, p_min_x = find_min_x(contours)
         c_max_x, p_max_x = find_max_x(contours)
@@ -370,8 +391,8 @@ while True:
         c_min_y, p_min_y = find_min_y(contours)
         x,y,w,h = cv2.boundingRect(contours[c_min_y][p_min_y])
 
-        hub_x = x
-        hub_y = y
+        hub_x = round(x + w/2)
+        hub_y = round(y + h/2)
 
         cam_distance = look_up_distance_y(hub_y) # if camera is in fixed position
         #min_x_x = contours[c_min_x_full_width][p_min_x_full_width][0][0]
@@ -380,17 +401,15 @@ while True:
         cam_angle_of_horizontal = calc_horizontal_angle_of(hub_x) # from new center
         cam_angle_of_vertical = calc_vertical_angle_of(hub_y) # from new center
 
-        pts = np.array([[first_min_x_x,first_min_x_y],[hub_x,hub_y],[first_max_x_x,first_max_x_y]], np.int32)
-        image = cv2.polylines(image, [pts], True, (0,0,255), 3)
-
         # loop time
         current_time = time.process_time()
         calc_time = current_time - start_time
         loops = loops + 1
 
-        # data format for network table key "Target" : "id,current time,calcuated time,distance,angle of,color"
-        hub_data = "{},{},{},{},{},{}".format(loops,current_time,calc_time,cam_distance,cam_angle_of_horizontal,0)
-        nt.putString(hub_data)
+        output_data(loops,current_time,calc_time,cam_distance,cam_angle_of_horizontal,0)
+
+        pts = np.array([[first_min_x_x,first_min_x_y],[hub_x,hub_y],[first_max_x_x,first_max_x_y]], np.int32)
+        image = draw_target(image, pts)
 
     # update all the images
     cv2.imshow("RPiVideo",image)