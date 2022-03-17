f49efabfae7e20e08d40f7df5581da9992e97fc4
GSchwartz23 <23gschwartz@bishopfoley.org>
2022-03-17 00:51:04 -0400

    add message before starting vision program

--- start_vision_cargo.sh
+++ start_vision_cargo.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #
 cd /home/pi/2022-FRC-Vision
+echo "starting vision cargo"
 sleep 3
 python3 vision_cargo.py debug output
-echo "ending vision"
+echo "ending vision cargo"
 sleep 3
 exit
